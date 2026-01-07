5263d0f649280c8e021ec7edf8836ea156f40cfe
manishsahu-vrize <manishsahu@vrize.com>
2026-01-07 15:06:56 +0530

    Centralized error handler

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+// Centralized error handler
+app.use((err, req, res, next) => {
+  return res
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Something went wrong!" });
+});
 module.exports = app;
